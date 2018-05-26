b138ea5f1a2339a5c77489191a0909267f1b4001
Dmitriy Ryajov <dryajov@gmail.com>
2018-05-26 10:57:42 -0600

    test: use random ports in tests

--- test/pmp.js
+++ test/pmp.js
@@ -8,33 +8,29 @@ const NatPMP = require('../src/mappers/pmp')
 chai.use(dirtyChai)
 const expect = chai.expect
 
+// TODO: provisional tests
+// need to figure out a more
+// robust way of testing this
 describe('NAT-PMP tests', () => {
   let natPMP
+  let natmapping
   before(() => {
     natPMP = new NatPMP()
   })
 
   it('should add mapping', (done) => {
-    natPMP.addMapping(50566, 50566, 0, {}, [], (error, mapping) => {
+    let port = ~~(Math.random() * 65536)
+    natPMP.addMapping(port, port, 0, {}, [], (error, mapping) => {
       expect(error).to.not.exist()
-      expect(mapping.internalPort).to.be.eql(50566)
+      expect(mapping.internalPort).to.be.eql(port)
+      natmapping = mapping
       done()
     })
   }).timeout(5 * 10000)
 
   it('should delete a mapping', (done) => {
-    let mapping = {
-      errInfo: null,
-      externalIp: '186.4.10.102',
-      externalPort: 50566,
-      internalIp: '10.0.0.107',
-      internalPort: 50566,
-      protocol: 'natPmp',
-      routerIp: '10.0.0.1',
-      ttl: 86400
-    }
-    natPMP.mappings[50566] = mapping
-    natPMP.deleteMapping(50566, {}, (error) => {
+    natPMP.mappings[natmapping.externalPort] = natmapping
+    natPMP.deleteMapping(natmapping.externalPort, {}, (error) => {
       expect(error).to.not.exist()
       done()
     })
--- test/upnp.js
+++ test/upnp.js
@@ -8,34 +8,29 @@ const NatUpnp = require('../src/mappers/upnp')
 chai.use(dirtyChai)
 const expect = chai.expect
 
+// TODO: provisional tests
+// need to figure out a more
+// robust way of testing this
 describe('Nat UpNP tests', () => {
   let natUpnp
+  let natmapping
   before(() => {
     natUpnp = new NatUpnp()
   })
 
   it('should add mapping', (done) => {
-    natUpnp.addMapping(50567, 50567, 0, {}, [], (error, mapping) => {
+    let port = ~~(Math.random() * 65536)
+    natUpnp.addMapping(port, port, 0, {}, [], (error, mapping) => {
       expect(error).to.not.exist()
-      expect(mapping.internalPort).to.be.eql(50567)
+      expect(mapping.internalPort).to.be.eql(port)
+      natmapping = mapping
       done()
     })
   }).timeout(5 * 10000)
 
   it('should delete a mapping', (done) => {
-    const mapping = {
-      errInfo: null,
-      externalIp: '186.4.10.102',
-      externalPort: 50567,
-      internalIp: '10.0.0.107',
-      internalPort: 50567,
-      nonce: null,
-      protocol: 'natPmp',
-      routerIp: '10.0.0.1',
-      ttl: 86400
-    }
-    natUpnp.mappings[50567] = mapping
-    natUpnp.deleteMapping(50567, {}, (error) => {
+    natUpnp.mappings[natmapping.externalPort] = natmapping
+    natUpnp.deleteMapping(natmapping.externalPort, {}, (error) => {
       expect(error).to.not.exist()
       done()
     })
